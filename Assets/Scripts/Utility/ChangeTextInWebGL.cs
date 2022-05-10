acad017366fd18f928b428c3f804daa777c89719
Luis Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2022-05-09 22:27:28 -0300

    Unity update and tweaks for WebGL build

---
+++ Assets/Scripts/Utility/ChangeTextInWebGL.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// Muda um texto em uma build Web GL
+[AddComponentMenu("Scripts/Utility/Change Text In Web GL")]
+public class ChangeTextInWebGL : MonoBehaviour {
+
+	public Text target;
+
+	public string textToReplace;
+
+	void Start () {
+		# if UNITY_WEBGL
+			target.text = textToReplace;
+		# endif
+	}
+}
